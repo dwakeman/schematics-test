bf2fbe7ecd37e3a9da0c0666a5614b8875c3cdf9
Dave Wakeman <dwakeman@us.ibm.com>
2019-08-30 15:34:08 -0500

    removed provider version var and hardcoded it

--- provider.tf
+++ provider.tf
@@ -4,11 +4,11 @@ variable "bluemix_api_key" {}
 variable "org_name" {}
 variable "space_name" {}
 
-variable "provider_version" {}
+
 
 provider "ibm" {
 bluemix_api_key    = "${var.bluemix_api_key}"
 softlayer_username = "${var.softlayer_username}"
 softlayer_api_key  = "${var.softlayer_api_key}"
-version            = "${var.provider_version}"
+version            = "<= 0.17.1"
 }
