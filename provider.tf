69ee7e0b84eff0ccc4b1fb07791c4a3ab1b902c7
Dave Wakeman <dwakeman@us.ibm.com>
2019-08-30 15:36:58 -0500

    updated provider info

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-variable "softlayer_username" {}
-variable "softlayer_api_key" {}
-variable "bluemix_api_key" {}
-variable "org_name" {}
-variable "space_name" {}
-
-
-
 provider "ibm" {
-bluemix_api_key    = "${var.bluemix_api_key}"
-softlayer_username = "${var.softlayer_username}"
-softlayer_api_key  = "${var.softlayer_api_key}"
-version            = "<= 0.17.1"
+    generation = 1
 }
