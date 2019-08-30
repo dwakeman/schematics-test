852ebabd62bdf3026d820d5b6b4123d688700e31
Dave Wakeman <dwakeman@us.ibm.com>
2019-08-30 15:31:19 -0500

    added provider version back

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ provider "ibm" {
 bluemix_api_key    = "${var.bluemix_api_key}"
 softlayer_username = "${var.softlayer_username}"
 softlayer_api_key  = "${var.softlayer_api_key}"
-
+version            = "${var.provider_version}"
 }
