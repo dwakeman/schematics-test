238efd7661f92c4e8ac4c49b2efb1e0363247fd2
Dave Wakeman <dwakeman@us.ibm.com>
2019-08-30 15:46:30 -0500

    added subnets

--- main.tf
+++ main.tf
@@ -14,6 +14,27 @@ resource "ibm_is_subnet" "subnet1" {
   }
 }
 
+resource "ibm_is_subnet" "subnet2" {
+  name            = "vpc2-subnet2"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone2}"
+  ipv4_cidr_block = "10.240.0.0/28"
 
+  provisioner "local-exec" {
+    command = "sleep 300"
+    when    = "destroy"
+  }
+}
 
+resource "ibm_is_subnet" "subnet3" {
+  name            = "vpc2-subnet3"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone3}"
+  ipv4_cidr_block = "10.240.0.0/28"
+
+  provisioner "local-exec" {
+    command = "sleep 300"
+    when    = "destroy"
+  }
+}
 
--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "zone1" {
 variable "zone2" {
   default = "us-south-2"
 }
+
+variable "zone3" {
+  default = "us-south-3"
+}
