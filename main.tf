a63473fb199e045195f13d50f292ad1c0dac8651
Dave Wakeman <dwakeman@us.ibm.com>
2019-08-30 14:23:06 -0500

    added provider info

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "ibm_is_vpc" "vpc1" {
-  name = "vpc1"
+resource "ibm_is_vpc" "vpc2-tf" {
+  name = "vpc2-tf"
 }
 
-resource "ibm_is_subnet" "subnet1" {
-  name            = "subnet1"
+resource "ibm_is_subnet" "vpc2-subnet1" {
+  name            = "vpc2-subnet1"
   vpc             = "${ibm_is_vpc.vpc1.id}"
   zone            = "${var.zone1}"
   ipv4_cidr_block = "10.240.0.0/28"
---
+++ provider.tf
@@ -0,0 +1,12 @@
+variable "softlayer_username" {}
+variable "softlayer_api_key" {}
+variable "bluemix_api_key" {}
+variable "org_name" {}
+variable "space_name" {}
+
+provider "ibm" {
+bluemix_api_key    = "${var.bluemix_api_key}"
+softlayer_username = "${var.softlayer_username}"
+softlayer_api_key  = "${var.softlayer_api_key}"
+
+}
