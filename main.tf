cd7321aa2fef1c741eac7cd79ce7d197a3fb1276
Dave Wakeman <dwakeman@us.ibm.com>
2019-09-02 19:04:39 -0500

    added variables for cidr blocks

--- main.tf
+++ main.tf
@@ -11,13 +11,18 @@ resource "ibm_is_vpc" "vpc1" {
   name = "${var.vpc_name}"
   resource_group  = "${var.resource_group}"
   tags = ["${var.environment}", "terraform"]
+
+  provisioner "local-exec" {
+      command = "ic resource groups"
+      when = "create"
+  }
 }
 
 resource "ibm_is_subnet" "subnet1" {
   name            = "${var.vpc_name}-subnet1"
   vpc             = "${ibm_is_vpc.vpc1.id}"
   zone            = "${var.zone1}"
-  ipv4_cidr_block = "10.240.0.0/28"
+  ipv4_cidr_block = "${var.cidr_block_1}"
 
   provisioner "local-exec" {
     command = "sleep 300"
@@ -29,7 +34,7 @@ resource "ibm_is_subnet" "subnet2" {
   name            = "${var.vpc_name}-subnet2"
   vpc             = "${ibm_is_vpc.vpc1.id}"
   zone            = "${var.zone2}"
-  ipv4_cidr_block = "10.240.64.0/28"
+  ipv4_cidr_block = "${var.cidr_block_2}"
 
   provisioner "local-exec" {
     command = "sleep 300"
@@ -41,7 +46,7 @@ resource "ibm_is_subnet" "subnet3" {
   name            = "${var.vpc_name}-subnet3"
   vpc             = "${ibm_is_vpc.vpc1.id}"
   zone            = "${var.zone3}"
-  ipv4_cidr_block = "10.240.128.0/18"
+  ipv4_cidr_block = "${var.cidr_block_3}"
 
   provisioner "local-exec" {
     command = "sleep 300"
--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "zone2" {
 variable "zone3" {
   default = "us-south-3"
 }
+
+variable cidr_block_1 {
+    default = "10.240.0.0/28"
+}
+
+variable cidr_block_2 {
+    default = "10.240.64.0/28"
+}
+
+variable cidr_block_3 {
+    default = "10.240.128.0/18"
+}
